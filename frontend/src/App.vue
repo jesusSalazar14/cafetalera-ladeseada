efec3a58356a1d0f34a87e10aa0bf31adc380fed
jesusSalazar14 <salazargodoyja@uvm.edu.ve>
2025-02-27 22:06:45 -0400

    componente registro

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
     <Lotes />
+    <Registro />
     <Login />
   </div>
 </template>
 
 <script setup>
 import Login from './components/login.vue';
+import Registro from './components/registro.vue'; 
+import Lotes from './components/lotes.vue'; 
+
 </script>
 
 <style scoped>
